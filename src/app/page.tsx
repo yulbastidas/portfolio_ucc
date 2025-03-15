647231943ea56f3b68aae6b1825903375fd3e07b
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-03-15 16:01:45 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen w-screen" style={{ backgroundColor: 'var(--bg-primary-lightest)' }}>
+      <h1 className="font-extrabold text-3xl" style={{ color: 'var(--bg-primary-darkest)' }}>
+        Yuly Tatiana Bastidas
+      </h1>
+    </main>
+  );
+}
